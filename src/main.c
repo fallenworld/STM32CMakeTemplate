5b8271d6069ad43cb52dd95d18baf80e2ab85bde
fallenworld <fallenworlder@gmail.com>
2025-02-21 10:22:51 +0800

    src: Add helpers.h.

--- src/main.c
+++ src/main.c
@@ -3,23 +3,7 @@
  */
 
 #include "stm32f10x.h"
-
-static void delay_us(uint32_t us)
-{
-    SysTick->LOAD = 72 * us;
-    SysTick->VAL = 0;
-    SysTick->CTRL = 5;
-    while (!(SysTick->CTRL & 0x00010000)) {}
-    SysTick->CTRL = 4;
-}
-
-static void delay_ms(uint32_t ms)
-{
-    while (ms--)
-    {
-        delay_us(1000);
-    }
-}
+#include "helpers.h"
 
 int main(void)
 {
